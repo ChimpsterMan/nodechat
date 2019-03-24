e436925d2a80e616223c95e216477eaaf2628fb0
Maris Usis <marisusisme@gmail.com>
2019-03-24 22:13:43 +0000

    online counter

--- chat.js
+++ chat.js
@@ -2,12 +2,23 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 2000;
+var connections = 0;
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
+  socket.on('newConnection', function(){
+    connections += 1;
+    io.sockets.emit('update', connections);
+  });
+  
+  socket.on('disconnect', function(){
+    connections -= 1;
+    io.sockets.emit('update', connections);
+  });
+  
   socket.on('chat message', function(msg){
     io.emit('message', msg);
   });
